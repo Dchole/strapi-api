dc6b32103df70013f3f78f203383a29ba51d5dcc
Dchole <derekoware47@gmail.com>
2020-09-26 00:46:14 +0000

    database congi

--- config/database.js
+++ config/database.js
@@ -1,19 +1,14 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.8psph.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-api'),
-        username: env('DATABASE_USERNAME', 'bunny'),
-        password: env('DATABASE_PASSWORD', 'halloween0'),
+        uri: "DATABASE_URI",
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: true,
       },
     },
   },
